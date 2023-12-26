13a7442263faca327c77c8b30ba373afbd5a31ae
aidarkhanov <dairaidarkhanov@gmail.com>
2023-12-26 22:40:40 +0200

    docs: add game description comment in main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
+// Main idea:
+// We have 3 players, 1 bot or a real person who conducts the game.
+// The game is conducted by providing a question and a word.
+// To guess a letter from a word,
+// each player rotates the wheel.
+// On each rotation, a player gets a score that lets to guess a letter,
+// otherwise, a player gets "bankrupt" or "skip" that turns a move to the next
+// player.
+// The winner alone gets to the final part of the game to guess another word
+// but has a right to guess 3 letters straightaway.
+// Then, before 1 minute passed, a player has to guess a word,
+// otherwise, it's a loss.
+
 console.log("This will implement Yoola WebSocket server.");
